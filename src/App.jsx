960d114390e7cc2aca6c1b4f9422ac8f7e4ae5c9
Md Anayet Ullah <109759412+Anayet6863@users.noreply.github.com>
2024-11-03 21:16:55 +0600

    All the file set up are completed.

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-5xl font-bold">Hello there</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-6xl text-red-700">From App</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
